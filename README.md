faee61858db00ab569c60dbea9fe2f6e3e7917ac
Alex Lavallee <73203142+lavalleeale@users.noreply.github.com>
2021-01-03 00:48:20 -0800

    Added .DS_Store to .gitignore Added optional filename argument

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 This app is meant to be a quick and easy way to search for occurences of a string in a textfile. It will also search for the string skipping letters in between.
 # Usage
 
-Simply enter java -jar /path/to/jarfile.jar stringtosearchfor and have a txt file named search.txt that you want to search through.
+Simply enter java -jar /path/to/jarfile.jar stringtosearchfor and have a txt file named search.txt that you want to search through. You can also enter a filename as an optional second argument.
--- src/main/java/com/axlav/Main.java
+++ src/main/java/com/axlav/Main.java
@@ -5,12 +5,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception{
+    	String filename = "search.txt";
     	if (args.length<1) {
     		System.out.println("Error: Requires 1 argument to determine what to search for");
     		System.exit(1);
+		} else if (args.length==2) {
+    		filename=args[2];
 		}
 
-		File file = new File("search.txt");
+		File file = new File(filename);
 		Scanner sc = new Scanner(file);
 		sc.useDelimiter("\\Z");
 		String textFile = sc.next();
@@ -22,7 +25,7 @@ public class Main {
 		int totalMatches = 0;
 		String target = args[0].toLowerCase().replaceAll("\\W", "");
 	    char[] targetChars = target.toCharArray();
-		System.out.println("Searching For: " + target);
+		System.out.printf("Searching for: %s in %s \n\n", target, filename);
 
 	    for (int i = 1; i < (chars.length/targetChars.length);i++) {
 	    	skipMatches = 0;
@@ -42,15 +45,15 @@ public class Main {
 	        if (skipMatches!=0) {
 				totalMatches+=skipMatches;
 			}
-			System.out.print("\rSkip: "+i+" | Total Matches: "+totalMatches);
-	        Thread.sleep(0,0);
+			System.out.printf("\rSkip: %s | Total Matches: %s", i, totalMatches);
+	        Thread.sleep(0);
 		}
 	    if (totalMatches==0) {
 	    	System.out.println("\nFinished: Zero Matches Found");
 		} else if (totalMatches>1){
 	    	System.out.println("\nFinished: " + totalMatches + " Matches Found");
 		} else {
-			System.out.println("\nFinished: " + totalMatches + " Match Found");
+			System.out.println("\n\nFinished: " + totalMatches + " Match Found");
 		}
     }
 }
